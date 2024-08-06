c9c7cd731cc5c05e6b6a09141b08240218012020
joaoburi <joaoburi64@gmail.com>
2024-08-06 18:11:59 -0300

    att

--- script.js
+++ script.js
@@ -29,76 +29,19 @@ buttons.forEach(button => {
 // Mostrar o primeiro conteúdo por padrão
 contents[0].classList.add('ativo');
 
+
+
+
+
+
 /*
-var btn2 = document.querySelector('#btn2')
-var card2 = document.querySelector('#card2')
-
-btn2.addEventListener('click', function() {
-    if(card2.style.display === 'block') {
-        card2.style.display = 'none';
-    } else {
-        card2.style.display = 'block';
-    }
-});
-
-
-function mostrarCardapio1() {
-    let cardapio = document.querySelector('#card1')
-    if (cardapio.classList.contains('aparece')) {
-        cardapio.classList.remove('aparece');
-        
-    } else {
-        cardapio.classList.add('aparece');
-        
-    }
-}
-
-
-
-
-
-
-https://www.youtube.com/watch?v=1w5SNKwBDKo
-
-
-
-function mostrarDiv(card) {
-    // Esconde todas as divs
-    const divs = document.querySelectorAll('div');
-    divs.forEach(div => {
-      div.style.display = 'none';
+$(document).ready(function(){
+    $(".item-menu").click(function(event){
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top
+      }, 500);
     });
-
-    // Mostra a div específica
-    const div = document.getElementById('div' + card);
-    div.style.display = 'block';
-  }
-
-function mostrarCardapio() {
-    let burgue1 = document.querySelector('#teste1')
-    const fazia = document.querySelector('#cardapio1')
-    if (fazia.classList.contains('aparece')) {
-        fazia.classList.remove('aparece');
-
-    } else {
-        fazia.classList.add('aparece');
-    }
-}
-
-/*
-const cardapio = document.querySelector('#cardapio');
-const btnTeste = document.getElementById('teste');
-
-btnTeste.addEventListener('click', () => {
-    const items = cardapio.querySelectorAll('.pratos');
-    const itemsArray = Array.from(items);
-
-    
-    itemsArray.sort(() => Math.random() - 0.5);
-
-
-    cardapio.innerHTML = '';
-    itemsArray.forEach(item => cardapio.appendChild(item));
-
 });
+
 */
